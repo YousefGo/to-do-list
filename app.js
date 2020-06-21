fe9eb06c05354344f19ef90dd29c054a1364d2b8
YousefGo <yousef0sarkar@gmail.com>
2020-06-21 10:08:46 +0300

    add weekend && weekday html pages

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app=express();
 
 const bodyParser = require("body-parser");
-
+app.set('view engine', 'ejs');
+// date 
+var today =new Date();
+var data  = today.getDay();
 app.get("/",function(req,res)
 {
-res.send('get methods works');
+   
+res.send('get methods works'+data);
 }
+
+
 )
 app.listen(3000,function()
 {
