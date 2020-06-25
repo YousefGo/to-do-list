4caba75ed03ff569ead23b291eb5cb22a00e5d49
YousefGo <yousef0sarkar@gmail.com>
2020-06-25 20:07:19 +0300

    add list in project

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+// requrie express file 
 const express = require("express");
-const app=express();
-
+// create new object off express
+const app = express();
+// require body-parser 
 const bodyParser = require("body-parser");
+// set ejs into view engine 
 app.set('view engine', 'ejs');
 // date 
-var today =new Date();
-var data  = today.getDay();
-app.get("/",function(req,res)
-{
-   
-res.send('get methods works'+data);
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+var today  = new Date();
+var StringDate=today.toLocaleDateString("en-US", options);
+var day = "";
+var items=[];
+app.use(bodyParser.urlencoded({extended:true}))
+app.get("/", function (req, res) {
+    /* 0 == sunday 
+       1= monday 
+       2=tuesday 
+       3=wednesday 
+       4= thures day 
+       5 = firday 
+       6= stauts day    
+    
+    */
+  
+    res.render("list", { sday:StringDate,items:items});
 }
 
 
 )
-app.listen(3000,function()
+app.post("/",function(req,res)
 {
+var item = req.body.num1;
+items.push(item);
+res.redirect("/");
+}
+)
+
+
+
+app.listen(3000, function () {
     console.log("work in port 3000");
 }
 )
